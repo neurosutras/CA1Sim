4c84bd557d170f6605d892bdf3dd0de49b9ebc05
Aaron D. Milstein <neurosutras@gmail.com>
2015-05-20 16:11:31 -0400

    051815 Preparing to scale up parallel methods to utilize multiple nodes on a cluster.

--- testMPI.sh
+++ testMPI.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #cd $HOME/.ipython/
-#cd $HOME/PycharmProjects/NEURON/
-cd $HOME/CA1Sim
+cd $HOME/PycharmProjects/NEURON/
+#cd $HOME/CA1Sim
 #ipcluster start -n 8 --ip='*' --profile=mpi &
-ipcluster start -n 8 --ip='*' --profile-dir=$HOME/.ipython/profile_default --engines=MPIEngineSetLauncher &
+#ipcluster mpiexec --n=8 --ip='*' --mpi=mpi4py --profile-dir=$HOME/.ipython/profile_default --engines=MPIEngineSetLauncher &
 #ipcluster start -n 32 --ip='*' --profile=mpi &
+ipcontroller --profile-dir=$HOME/.ipython/profile_default &
+sleep 30
+mpirun -n 8 ipengine --profile-dir=$HOME/.ipython/profile_default &
 sleep 60
 #cd $HOME/CA1Sim/
 python testMPI_controller.py
 #cd $HOME/.ipython/
 #ipcluster stop --profile=mpi
-ipcluster stop
+ipcluster stop --profile-dir=$HOME/.ipython/profile_default
