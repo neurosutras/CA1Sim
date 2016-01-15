ad013419a1b3be0b7a1b5bb7c3473f0e8eb24aa4
Aaron D. Milstein <neurosutras@gmail.com>
2016-01-15 16:23:40 -0500

    011516 - Running additional simulations without NMDARs.

--- batch_modulate_input_no_nmda.sh
+++ batch_modulate_input_no_nmda.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #cd $HOME/PycharmProjects/NEURON/
 cd $HOME/CA1Sim
-qsub -N job_011116_e2000_i600_no_nmda_inh0_101 -m e -M milsteina@janelia.hhmi.org -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 2000 600 0 101'
-qsub -N job_011116_e2000_i600_no_nmda_inh0_102 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 2000 600 0 102'
-qsub -N job_011116_e2000_i600_no_nmda_inh0_103 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 2000 600 0 103'
-qsub -N job_011116_e2400_i600_no_nmda_inh0_110 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 2400 600 0 110'
-qsub -N job_011116_e2400_i600_no_nmda_inh0_111 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 2400 600 0 111'
+qsub -N job_011516_e3200_i600_no_nmda_inh0_120 -m e -M milsteina@janelia.hhmi.org -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 3200 600 0 120'
+qsub -N job_011516_e3200_i600_no_nmda_inh0_121 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 3200 600 0 121'
+qsub -N job_011516_e3200_i600_no_nmda_inh0_122 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 3200 600 0 122'
+qsub -N job_011516_e3200_i600_no_nmda_inh0_123 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 3200 600 0 123'
+qsub -N job_011516_e3600_i600_no_nmda_inh0_130 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 3600 600 0 130'
+qsub -N job_011516_e3600_i600_no_nmda_inh0_131 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 3600 600 0 131'
+qsub -N job_011516_e3600_i600_no_nmda_inh0_132 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 3600 600 0 132'
+qsub -N job_011516_e3600_i600_no_nmda_inh0_133 -b y -cwd -V 'python test_poisson_inputs_no_nmda.py 3 3600 600 0 133'
