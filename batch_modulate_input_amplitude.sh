43b6058baf92a4c7ca8486627271ea9fc909e606
Aaron D. Milstein <neurosutras@gmail.com>
2016-01-30 15:48:23 -0500

    013016 - Re-calibrating excitatory input number for patterned input simulation with new mechanism dictionary.

--- batch_modulate_input_amplitude.sh
+++ batch_modulate_input_amplitude.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #cd $HOME/PycharmProjects/NEURON/
 cd $HOME/CA1Sim
-qsub -N job_013016_e2200_i600_subt_modinh0_0 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2200 600 0 0'
-qsub -N job_013016_e2200_i600_subt_modinh0_1 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2200 600 0 1'
-qsub -N job_013016_e2400_i600_subt_modinh0_2 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2400 600 0 2'
-qsub -N job_013016_e2400_i600_subt_modinh0_3 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2400 600 0 3'
-qsub -N job_013016_e2600_i600_subt_modinh0_4 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2600 600 0 4'
-qsub -N job_013016_e2600_i600_subt_modinh0_5 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2600 600 0 5'
-qsub -N job_013016_e2800_i600_subt_modinh0_6 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2800 600 0 6'
-qsub -N job_013016_e2800_i600_subt_modinh0_7 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2800 600 0 7'
-qsub -N job_013016_e3000_i600_subt_modinh0_8 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 3000 600 0 8'
-qsub -N job_013016_e3000_i600_subt_modinh0_9 -l haswell=true -m e -M milsteina@janelia.hhmi.org -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 3000 600 0 9'
+qsub -N job2_013016_e2200_i400_subt_modinh0_10 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2200 400 0 10'
+qsub -N job2_013016_e2200_i400_subt_modinh0_11 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2200 400 0 11'
+qsub -N job2_013016_e2400_i400_subt_modinh0_12 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2400 400 0 12'
+qsub -N job2_013016_e2400_i400_subt_modinh0_13 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2400 400 0 13'
+qsub -N job2_013016_e2600_i400_subt_modinh0_14 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2600 400 0 14'
+qsub -N job2_013016_e2600_i400_subt_modinh0_15 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2600 400 0 15'
+qsub -N job2_013016_e1800_i400_subt_modinh0_16 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 1800 400 0 16'
+qsub -N job2_013016_e1800_i400_subt_modinh0_17 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 1800 400 0 17'
+qsub -N job2_013016_e2000_i400_subt_modinh0_18 -l haswell=true -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2000 400 0 18'
+qsub -N job2_013016_e2000_i400_subt_modinh0_19 -l haswell=true -m e -M milsteina@janelia.hhmi.org -b y -cwd -V 'python test_poisson_inputs_subtractive_modinh.py 0 2000 400 0 19'
