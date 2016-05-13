d1bf66db80789154ae20bc2db0c5933aeae55782
Aaron D. Milstein <neurosutras@gmail.com>
2016-05-13 15:57:05 -0400

    051316: A few things to process for supplementary figures: gradients of R_inp, EPSP amp and decay, spine-to-branch attenuation.

---
+++ batch_spine_attenuation_ratio.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#cd $HOME/PycharmProjects/NEURON/
+cd $HOME/CA1Sim
+cluster_id="$1"
+ipcluster start -n 31 --profile-dir=$HOME/.ipython/profile_default --cluster-id=$cluster_id &
+sleep 180
+ipython parallel_spine_attenuation_ratio_controller.py $cluster_id
+ipcluster stop --profile-dir=$HOME/.ipython/profile_default --cluster-id=$cluster_id
