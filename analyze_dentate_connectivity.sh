0e3b277cb4dfe3bcea06eacb1db7bc2420cf57e8
Aaron D. Milstein <neurosutras@gmail.com>
2016-10-07 15:57:06 -0700

    100716: Dev branch: Parallel scipts for computing dentate model grid cell rate maps.

--- analyze_dentate_connectivity.sh
+++ analyze_dentate_connectivity.sh
@@ -2,7 +2,7 @@
 #cd $HOME/PycharmProjects/NEURON/
 cd $HOME/CA1Sim_dev
 cluster_id="$1"
-ipcluster start -n 31 --profile-dir=$HOME/.ipython/profile_default --cluster-id=$cluster_id &
+ipcluster start -n 63 --profile-dir=$HOME/.ipython/profile_default --cluster-id=$cluster_id &
 sleep 180
 ipython analyze_dentate_connectivity_controller.py $cluster_id
 ipcluster stop --profile-dir=$HOME/.ipython/profile_default --cluster-id=$cluster_id
