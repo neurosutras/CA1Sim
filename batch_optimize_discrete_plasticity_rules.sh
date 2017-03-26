15ddb912759ca7fb637f7cfb8b94a95c291d604d
neurosutras <neurosutras@gmail.com>
2017-03-26 12:54:48 -0700

    032617: Dev branch: Extracting a saturation factor from the discrete version of the plasticity rule.

--- batch_optimize_discrete_plasticity_rules.sh
+++ batch_optimize_discrete_plasticity_rules.sh
@@ -2,9 +2,9 @@
 cd $HOME/CA1Sim_dev
 for i in 1 2 4 5 6 7 8 9 10 11 12 13 14 15 17 18 19 20 21 22 23
 do
-    qsub -pe batch 4 -N export_discrete_long_032617_cell$i -l d_rt=1200 -b y -cwd -V 'python optimize_discrete_long_plasticity_rule_032017.py $i'
+    qsub -pe batch 4 -N export_discrete_long_032617_cell$i -l d_rt=1200 -b y -cwd -V 'python optimize_discrete_long_plasticity_rule_032017.py ${i}'
 done
 for i in 1 2 3 4 5 6 7
 do
-    qsub -pe batch 4 -N export_discrete_long_032617_cell$i -l d_rt=1200 -b y -cwd -V 'python optimize_discrete_long_plasticity_rule_spont_032017.py $i'
+    qsub -pe batch 4 -N export_discrete_long_032617_cell$i -l d_rt=1200 -b y -cwd -V 'python optimize_discrete_long_plasticity_rule_spont_032017.py ${i}'
 done
