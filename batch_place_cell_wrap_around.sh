bab1163696fb40c8f469f8a2a09b90b2b53e1ce7
neurosutras <neurosutras@gmail.com>
2017-03-29 12:00:37 -0700

    032917: Dev branch: Simulating place cells with variable DC soma offset for Ching-lung.

--- batch_place_cell_wrap_around.sh
+++ batch_place_cell_wrap_around.sh
@@ -2,11 +2,12 @@
 #cd $HOME/PycharmProjects/NEURON/
 cd $HOME/CA1Sim_dev
 date = 032917
+export date
 j=0
 for ttx in 0 1
 do
     export ttx
-    for i in -0.1, -0.075, -0.05, -0.025, 0., 0.025, 0.05, 0.075, 0.1, 0.125, 0.150, 0.175, 0.2, 0.225, 0.25
+    for i in -0.1 -0.075 -0.05 -0.025 0. 0.025 0.05 0.075 0.1 0.125 0.150 0.175 0.2 0.225 0.25
     do
         export j
         export i
