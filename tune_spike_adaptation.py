540330ecbfdd3fd84c1e4ab7b22b29e90caec04f
Grace Ng <graceyng14@gmail.com>
2017-04-04 10:53:12 -0700

    Merge remote-tracking branch 'remotes/origin/dev' into dev
    # Conflicts:
    #	parallel_optimize_spike_stability_controller.py

--- tune_spike_adaptation.py
+++ tune_spike_adaptation.py
@@ -20,22 +20,22 @@ neurotree_dict = read_from_pkl(morph_dir+neurotree_filename)
 rec_filename = str(time.strftime('%m%d%Y', time.gmtime()))+'_'+str(time.strftime('%H%M%S', time.gmtime()))+\
                '_pid'+str(os.getpid())+'_sim_output'
 
+#Is this necessary?
 x = None  # Placeholder for parameters pushed from controller.
-
-""""
-#[soma.Ca factor, soma.CadepK factor]
-x['spike_adaptation'] = [0.1, 0.1]
-"""
 
 xmin = {}
 xmax = {}
 
+#Update xmin and xmax!
+"""
 soma_na_gbar = 0.04
 # [soma.gkabar, soma.gkdrbar, axon.gkabar_kap factor, axon.gbar_nax factor, soma.sh_nas/x, axon.gkdrbar factor,
 #  dend.gkabar factor]
 xmin['na_ka_stability'] = [0.01, 0.01, 1., 2., 0.1, 1., 1.]
 xmax['na_ka_stability'] = [0.075, 0.05, 5., 5., 6., 2., 5.]
+"""
 
+#Check bounds to make sure soma gKm and axon gKm are within a reasonable range
 check_bounds = CheckBounds(xmin, xmax)
 
 if len(sys.argv) > 1:
@@ -48,97 +48,13 @@ else:
     mech_filename = '030217 GC optimizing excitability'
 
 
-@interactive
-def get_spike_shape(vm):
-    """
-
-    :param t: array
-    :param vm: array
-    :return: tuple of float: (v_peak, th_v, ADP, AHP)
-    """
-    vm = vm[int((equilibrate+1.)/dt):]
-    dvdt = np.gradient(vm, [dt])
-    th_x = np.where(dvdt > th_dvdt)[0]
-    if th_x.any():
-        th_x = th_x[0] - int(1.6/dt)
-    else:
-        th_x = np.where(vm > -30.)[0][0] - int(2./dt)
-    th_v = vm[th_x]
-    v_before = np.mean(vm[th_x-int(0.1/dt):th_x])
-    v_peak = np.max(vm[th_x:th_x+int(5./dt)])
-    x_peak = np.where(vm[th_x:th_x+int(5./dt)] == v_peak)[0][0]
-    v_AHP = np.min(vm[th_x+x_peak:th_x+int(12./dt)])
-    x_AHP = np.where(vm[th_x+x_peak:th_x+int(12./dt)] == v_AHP)[0][0]
-    AHP = v_before - v_AHP
-    # if spike waveform includes an ADP before an AHP, return the value of the ADP in order to increase error function
-    rising_x = np.where(dvdt[th_x+x_peak:th_x+x_peak+x_AHP] > 0.)[0]
-    if rising_x.any():
-        v_ADP = np.max(vm[th_x+x_peak+rising_x[0]:th_x+x_peak+x_AHP])
-        ADP = v_ADP - v_AHP
-    else:
-        ADP = 0.
-    return v_peak, th_v, ADP, AHP
-
-
-@interactive
-def offset_vm(description, vm_target=None):
-    """
-
-    :param description: str
-    :param vm_target: float
-    """
-    if vm_target is None:
-        vm_target = v_init
-    sim.modify_stim(0, amp=0.)
-    node = rec_nodes[description]
-    loc = rec_locs[description]
-    rec_dict = sim.get_rec(description)
-    sim.modify_stim(1, node=node, loc=loc, amp=0.)
-    rec = rec_dict['vec']
-    offset = True
-    sim.tstop = equilibrate
-    t = np.arange(0., equilibrate, dt)
-    sim.modify_stim(1, amp=i_holding[description])
-    sim.run(vm_target)
-    vm = np.interp(t, sim.tvec, rec)
-    v_rest = np.mean(vm[int((equilibrate - 3.)/dt):int((equilibrate - 1.)/dt)])
-    initial_v_rest = v_rest
-    if v_rest < vm_target - 0.5:
-        i_holding[description] += 0.01
-        while offset:
-            if sim.verbose:
-                print 'increasing i_holding to %.3f (%s)' % (i_holding[description], description)
-            sim.modify_stim(1, amp=i_holding[description])
-            sim.run(vm_target)
-            vm = np.interp(t, sim.tvec, rec)
-            v_rest = np.mean(vm[int((equilibrate - 3.)/dt):int((equilibrate - 1.)/dt)])
-            if v_rest < vm_target - 0.5:
-                i_holding[description] += 0.01
-            else:
-                offset = False
-    elif v_rest > vm_target + 0.5:
-        i_holding[description] -= 0.01
-        while offset:
-            if sim.verbose:
-                print 'decreasing i_holding to %.3f (%s)' % (i_holding[description], description)
-            sim.modify_stim(1, amp=i_holding[description])
-            sim.run(vm_target)
-            vm = np.interp(t, sim.tvec, rec)
-            v_rest = np.mean(vm[int((equilibrate - 3.)/dt):int((equilibrate - 1.)/dt)])
-            if v_rest > vm_target + 0.5:
-                i_holding[description] -= 0.01
-            else:
-                offset = False
-    sim.tstop = duration
-    return v_rest
-
-
+#Change to update_spike_adaptation
 @interactive
 def update_mech_dict():
     update_na_ka_stability(x)
     cell.export_mech_dict(cell.mech_filename)
 
-
+#Change to update_spike_adaptation
 @interactive
 def update_na_ka_stability(x):
     """
@@ -171,59 +87,7 @@ def update_na_ka_stability(x):
         cell.modify_mech_param(sec_type, 'kap', 'gkabar', x[0] * x[2])
         cell.modify_mech_param(sec_type, 'nax', 'sh', origin='axon_hill')
 
-
-@interactive
-def compute_spike_shape_features(local_x=None, plot=0):
-    """
-    :param local_x: array [soma.gkabar, soma.gkdrbar, axon.gkabar_kap factor, axon.gbar_nax factor, soma.sh_nas/x,
-                    axon.gkdrbar factor, dend.gkabar factor]
-    :return: float
-    """
-    if local_x is None:
-        local_x = x
-    if not check_bounds.within_bounds(local_x, 'na_ka_stability'):
-        print 'Process %i: Aborting - Parameters outside optimization bounds.' % (os.getpid())
-        return None
-    start_time = time.time()
-    update_na_ka_stability(local_x)
-    soma_vm = offset_vm('soma', v_active)
-    result = {'v_rest': soma_vm}
-    sim.modify_stim(0, node=cell.tree.root, loc=0., dur=100.)
-    duration = equilibrate + 200.
-    sim.tstop = duration
-    t = np.arange(0., duration, dt)
-    spike = False
-    d_amp = 0.01
-    amp = i_th['soma'] - d_amp
-    while not spike:
-        sim.modify_stim(0, amp=amp)
-        sim.run(v_active)
-        vm = np.interp(t, sim.tvec, sim.get_rec('soma')['vec'])
-        if amp == 0.05 and np.any(vm[:int(equilibrate/dt)] > -30.):
-            print 'Process %i: Aborting - spontaneous firing' % (os.getpid())
-            return None
-        if np.any(vm[int(equilibrate/dt):int((equilibrate+50.)/dt)] > -30.):
-            spike = True
-        else:
-            amp += d_amp
-            if sim.verbose:
-                print 'increasing amp to %.3f' % amp
-    i_th['soma'] = amp
-    peak, threshold, ADP, AHP = get_spike_shape(vm)
-    dend_vm = np.interp(t, sim.tvec, sim.get_rec('dend')['vec'])
-    th_x = np.where(vm[int(equilibrate / dt):] >= threshold)[0][0] + int(equilibrate / dt)
-    dend_peak = np.max(dend_vm[th_x:th_x + int(10. / dt)])
-    dend_pre = np.mean(dend_vm[th_x - int(0.2 / dt):th_x - int(0.1 / dt)])
-    result['dend_amp'] = (dend_peak - dend_pre) / (peak - threshold)
-    print 'Process %i took %.1f s to find spike rheobase at amp: %.3f' % (os.getpid(), time.time() - start_time, amp)
-    if plot:
-        sim.plot()
-    result['v_th'] = threshold
-    result['ADP'] = ADP
-    result['AHP'] = AHP
-    result['amp'] = amp
-    return result
-
+#Update so that it no longer uses x values from the program!
 @interactive
 def compute_spike_stability_features(amp, local_x=None, plot=0):
     sim.parameters['amp'] = amp
@@ -261,7 +125,7 @@ axon_th=20.
 start=250
 stop=370
 """
-def sim_spike_times(amp, axon_th, start, stop, local_x=None):
+def sim_spike_times(amp, axon_th=10, start=250, stop=360, local_x=None):
     """
 
     :param amp: float
@@ -318,16 +182,34 @@ def adapt_index(spike_times):
     adi /= count
     return adi
 
-def adjust_spike_number(target_spikes, axon_th, start, stop):
+def adjust_spike_number(target_spikes, axon_th=10, start=250, stop=360):
     spike_num = 0
-    amp = 0.25
+    spikes_amp_corr = []
+    d_amp = 0.01
+    amp = i_th['soma'] - d_amp
+    while spike_num < 1:
+        spike_times = sim_spike_times(amp, axon_th, start, stop)
+        spike_num = len(spike_times)
+        amp += 0.01
+    spikes_amp_corr.append([amp, spike_num])
     while spike_num < target_spikes:
         spike_times = sim_spike_times(amp, axon_th, start, stop)
         spike_num = len(spike_times)
-        amp += 0.05
+        amp += 0.01
+    spikes_amp_corr.append([amp, spike_num])
+    for amp_incr in [0.05, 0.1]:
+        test_amp = amp + amp_incr
+        test_spike_times = sim_spike_times(amp, axon_th, start, stop)
+        spikes_amp_corr.append([test_amp, len(test_spike_times)])
+    return spike_times, amp
 
-def spike_adaptation_error(spike_num):
-    amp = adjust_spike_number(spike_num)
+def spike_adaptation_error(spike_num, axon_th, start, stop):
+    spike_times, amp = adjust_spike_number(spike_num, axon_th, start, stop)
+    #Consider error from adaptation index and the error from the correlation of the spike count v. current injection amp,
+    #and the error from the desired slope of the spike count v. current injection
+    #Calculate overall error: sum of all (target-test)^2/desired_variance
+
+    # Target adaptation index: 0.118989 for ~6 spikes
 
 
 @interactive
@@ -392,7 +274,14 @@ else:
 orig_ka_soma_gkabar = cell.mech_dict['soma']['kap']['gkabar']['value']
 orig_ka_dend_gkabar = orig_ka_soma_gkabar + orig_ka_dend_slope * 300.
 
-x = [0.0308, 0.0220, 4.667, 4.808, 4.032, 1.297, 1.023]
+# These values will now be saved in the mech dictionary that is updated by previous round of optimization
+#x = [0.0308, 0.0220, 4.667, 4.808, 4.032, 1.297, 1.023]
+
+
+#[soma.gCa factor, soma.gCadepK factor, soma.gKm, axon(ais, hill).gKm factor]
+#Need to adjust the gKm values!!
+x = [0.1, 0.1, 0.1, 1]
+
 
 sim.append_rec(cell, cell.tree.root, loc=0.5, object=cell.tree.root.sec(0.5), param='_ref_ina_nas',
                description='Soma nas_i')
@@ -419,4 +308,3 @@ sim.append_rec(cell, cell.tree.root, loc=0.5, object=cell.tree.root.sec(0.5), pa
 sim.append_rec(cell, cell.tree.root, loc=0.5, object=cell.tree.root.sec(0.5), param='_ref_i_CadepK',
                description='Soma CadepK_i')
 
-#Target adaptation index: 0.118989 for ~6 spikes
