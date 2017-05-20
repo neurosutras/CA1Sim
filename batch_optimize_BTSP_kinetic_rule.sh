016846842c596daabceae9e423cf7a7640a61be4
Aaron Milstein <aaronmil@sherlock-ln01.stanford.edu>
2017-05-20 16:48:21 -0700

    052017: dev branch: Added bash job script

---
+++ batch_optimize_BTSP_kinetic_rule.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH -J optimize_BTSP_kinetic_rule
+#SBATCH -o optimize_BTSP_kinetic_rule_052017.%j.o
+#SBATCH -n 31
+#SBATCH -t 48:00:00
+#SBATCH --mem-per-cpu=4G
+##SBATCH --mail-user=graceyng@stanford.edu
+#SBATCH --mail-user=aaronmil@stanford.edu
+#SBATCH --mail-type=END
+#SBATCH --mail-type=BEGIN
+#
+set -x
+
+cd $HOME/CA1Sim
+data_dir="data/"
+cluster_id="$1"
+ipcontroller --ip='*' --quiet --cluster-id=$cluster_id &
+sleep 60
+mpirun -np 30 ipengine --quiet --cluster-id=$cluster_id &
+sleep 60
+python parallel_optimize_BTSP_kinetic_rule_controller_052017.py "$data_dir" $cluster_id
