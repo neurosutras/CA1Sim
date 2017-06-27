00ec11068264647c6d00187cecb8880380beb243
Grace Yan-Yee Ng <graceyng@stanford.edu>
2017-06-27 11:33:15 -0700

    adding batch script for leak

---
+++ batch_optimize_leak.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH -J optimize_leak
+#SBATCH -o optimize_leak_062717.%j.o
+#SBATCH -N 38
+#SBATCH -t 24:00:00
+#SBATCH --mem-per-cpu=4G
+#SBATCH --mail-user=graceyng@stanford.edu
+##SBATCH --mail-user=aaronmil@stanford.edu
+#SBATCH --mail-type=END
+#SBATCH --mail-type=BEGIN
+#
+set -x
+
+cd $HOME/CA1Sim
+cluster_id="$1"
+group_sz="$2"
+pop_size="$3"
+max_gens="$4"
+ipcontroller --ip='*' --quiet --cluster-id=$cluster_id &
+sleep 60
+mpirun -np 2 ipengine --quiet --cluster-id=$cluster_id &
+sleep 60
+python parallel_optimize_leak.py --cluster-id $cluster_id --group-size $group_sz --pop-size $pop_size --max-gens $max_gens
