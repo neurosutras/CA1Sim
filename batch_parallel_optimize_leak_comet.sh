8c5fecd772f58b3b2c83babca827aeeaf81fb952
Aaron D Milstein <aaronmil@comet-ln3.sdsc.edu>
2017-07-13 17:16:47 -0700

    071317 updating batch script on comet

--- batch_parallel_optimize_leak_comet.sh
+++ batch_parallel_optimize_leak_comet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J parallel_optimize_leak_comet
 #SBATCH -o parallel_optimize_leak_comet_071317.%j.o
-#SBATCH -e optimize_leak_comet_071317.%j.e
+#SBATCH -e parallel_optimize_leak_comet_071317.%j.e
 #SBATCH -N 26 -n 602
-#SBATCH -t 00:30:00
+#SBATCH -t 04:00:00
 ##SBATCH --mail-user=graceyng@stanford.edu
 #SBATCH --mail-user=aaronmil@stanford.edu
 #SBATCH --mail-type=END
@@ -13,7 +13,7 @@
 set -x
 
 cd $HOME/CA1Sim
-cluster_id="test_comet"
+cluster_id="leak"
 
 ibrun -n 1 ipcontroller --ip='*' --cluster-id=$cluster_id &
 sleep 1
@@ -21,4 +21,4 @@ sleep 60
 ibrun -n 600 ipengine --cluster-id=$cluster_id &
 sleep 1
 sleep 180
-ibrun -n 1 python parallel_optimize_leak.py --cluster-id=$cluster_id --group-size=3 --pop-size=200 --max-iter=2 --path-length=2 --disp
+ibrun -n 1 python parallel_optimize_leak.py --cluster-id=$cluster_id --group-size=3 --pop-size=200 --max-iter=50 --path-length=3 --disp --export
