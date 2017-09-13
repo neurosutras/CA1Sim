816589b05ac9ebbc83dd522cb865c4986842259c
Aaron D Milstein <aaronmil@comet-ln2.sdsc.edu>
2017-09-13 15:18:30 -0700

    091317 dev branch: Merging changes to Comet job script

--- batch_optimize_synaptic_integration.sh
+++ batch_optimize_synaptic_integration.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -J parallel_optimize_synaptic_integration
-#SBATCH -o parallel_optimize_synaptic_integration_091217.%j.o
-#SBATCH -e parallel_optimize_synaptic_integration_091217.%j.e
+#SBATCH -o parallel_optimize_synaptic_integration_091317.%j.o
+#SBATCH -e parallel_optimize_synaptic_integration_091317.%j.e
 #SBATCH --partition=compute
-#SBATCH -N 84 -n 2002
+#SBATCH -N 71 -n 1682
 #SBATCH -t 24:00:00
 ##SBATCH --mail-user=graceyng@stanford.edu
 #SBATCH --mail-user=aaronmil@stanford.edu
@@ -18,10 +18,10 @@ export DATE=$(date +%Y%m%d_%H%M%S)
 export SCRATCH=/oasis/scratch/comet/$USER/temp_project
 cluster_id="synaptic_$DATE"
 
-ibrun -n 1 ipcontroller --ip='*' --DictDB.record_limit=4096 --DictDB.size_limit=3221225472 --DictDB.cull_fraction=0.05 --cluster-id=$cluster_id &
+ibrun -n 1 ipcontroller --ip='*' --nodb --cluster-id=$cluster_id &
 sleep 1
 sleep 60
-ibrun -n 2000 ipengine --mpi=mpi4py --cluster-id=$cluster_id &
+ibrun -n 1680 ipengine --mpi=mpi4py --cluster-id=$cluster_id &
 sleep 1
 sleep 180
-ibrun -n 1 python parallel_optimize.py --param-file-path='data/parallel_optimize_synaptic_config.yaml' --cluster-id=$cluster_id --pop-size=200 --max-iter=50 --path-length=3 --disp --export --output-dir=$SCRATCH
+ibrun -n 1 python parallel_optimize.py --param-file-path='data/parallel_optimize_synaptic_config.yaml' --cluster-id=$cluster_id --pop-size=200 --max-iter=50 --path-length=3 --disp --export --output-dir=$SCRATCH --hot-start --storage-file-path=$SCRATCH/091220171311_synaptic_integration_BGen_optimization_history.hdf5
