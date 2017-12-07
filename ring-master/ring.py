e74045bca0c3f205d7007420503f370a519cb9c0
Grace Ng <graceyng14@gmail.com>
2017-12-06 17:41:38 -0800

    120617 dev branch: Built a simple parallel optimization test script for parallel context and ring networks.

--- ring-master/ring.py
+++ ring-master/ring.py
@@ -1,23 +1,28 @@
 from neuron import h
 h.load_file('nrngui.hoc')
-pc = h.ParallelContext()
-rank = int(pc.id())
-nhost = int(pc.nhost())
 from cell import BallStick
 
 class Ring(object):
 
-  def __init__(self, ncell, delay):
+  def __init__(self, ncell, delay, this_pc):
     #print "construct ", self
+    global pc
+    pc = this_pc
+    global rank
+    rank = int(pc.id())
+    global nhost
+    nhost = int(pc.nhost())
     self.delay = delay
     self.ncell = int(ncell)
     self.mkring(self.ncell)
     self.mkstim()
     self.spike_record()
 
+  """
   def __del__(self):
     pc.gid_clear()
     #print "delete ", self
+  """
 
   def mkring(self, ncell):
     self.mkcells(ncell)
@@ -67,8 +72,7 @@ class Ring(object):
       pc.spike_record(nc.srcgid(), self.tvec, self.idvec)
 
 
-def runring(ncell=5, delay=1, tstop=100):
-  ring = Ring(ncell, delay)
+def runring(ring, ncell=5, delay=1, tstop=100):
   pc.set_maxstep(10)
   h.stdinit()
   pc.psolve(tstop)
@@ -79,89 +83,5 @@ def runring(ncell=5, delay=1, tstop=100):
   pc.allgather(ring.tvec.x[-1], tt)
   pc.allgather(ring.idvec.x[-1], idv)
   idmax = int(idv.x[int(tt.max_ind())])
-  return (int(spkcnt), tmax, idmax, (ncell, delay, tstop, (pc.id_world(), pc.nhost_world())))
-
-"""
-from neuron import h
-h.load_file('nrngui.hoc')
-#pc = h.ParallelContext()
-from cell import BallStick
-
-class Ring(object):
-
-  def __init__(self, ncell, delay, pc):
-    #print "construct ", self
-    self.pc = pc
-    self.rank = int(pc.id())
-    self.nhost = int(pc.nhost())
-    self.delay = delay
-    self.ncell = int(ncell)
-    self.mkring(self.ncell, self.rank, self.nhost)
-    self.mkstim()
-    self.spike_record()
-
-
-  def __del__(self):
-    self.pc.gid_clear()
-    #print "delete ", self
-
-  def mkring(self, ncell, rank, nhost):
-    self.mkcells(ncell, rank, nhost)
-    self.connectcells(ncell)
-
-  def mkcells(self, ncell, rank, nhost):
-    self.cells = []
-    for i in range(rank, ncell, nhost):
-      cell = BallStick()
-      self.cells.append(cell)
-      self.pc.set_gid2node(i, rank)
-      nc = cell.connect2target(None)
-      self.pc.cell(i, nc)
-
-  def connectcells(self, ncell):
-    self.nclist = []
-    # not efficient but demonstrates use of pc.gid_exists
-    for i in range(ncell):
-      targid = (i+1)%ncell
-      if self.pc.gid_exists(targid):
-        target = self.pc.gid2cell(targid)
-        syn = target.synlist[0]
-        nc = self.pc.gid_connect(i, syn)
-        self.nclist.append(nc)
-        nc.delay = self.delay
-        nc.weight[0] = 0.01
-
-
-  #Instrumentation - stimulation and recording
-  def mkstim(self):
-    if not self.pc.gid_exists(0):
-      return
-    self.stim = h.NetStim()
-    self.stim.number = 1
-    self.stim.start = 0
-    self.ncstim = h.NetCon(self.stim, self.pc.gid2cell(0).synlist[0])
-    self.ncstim.delay = 0
-    self.ncstim.weight[0] = 0.01
-
-  def spike_record(self):
-    self.tvec = h.Vector()
-    self.idvec = h.Vector()
-    for i in range(len(self.cells)):
-      nc = self.cells[i].connect2target(None)
-      self.pc.spike_record(nc.srcgid(), self.tvec, self.idvec)
-
-
-def runring(ncell=5, delay=1, tstop=100):
-  ring = Ring(ncell, delay)
-  ring.pc.set_maxstep(10)
-  h.stdinit()
-  ring.pc.psolve(tstop)
-  spkcnt = ring.pc.allreduce(len(ring.tvec), 1)
-  tmax = ring.pc.allreduce(ring.tvec.x[-1], 2)
-  tt = h.Vector()
-  idv = h.Vector()
-  ring.pc.allgather(ring.tvec.x[-1], tt)
-  ring.pc.allgather(ring.idvec.x[-1], idv)
-  idmax = int(idv.x[int(tt.max_ind())])
-  return (int(spkcnt), tmax, idmax, (ncell, delay, tstop, (ring.pc.id_world(), ring.pc.nhost_world())))
-"""
+  #return (int(spkcnt), tmax, idmax, (ncell, delay, tstop, (pc.id_world(), pc.nhost_world())))
+  return {'spkcnt': int(spkcnt), 'tmax': tmax, 'idmax': idmax, 'id_world': pc.id_world()}
