b983df623c599bf30458e1c73d0208462ba299c7
Aaron D. Milstein <neurosutras@gmail.com>
2018-01-30 15:39:31 -0800

    20180130: BTSP_dev branch: Merging changes on Cori

--- batch_parallel_optimize_BTSP_CA1_Cori_full.sh
+++ batch_parallel_optimize_BTSP_CA1_Cori_full.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -l
 
 #SBATCH -J parallel_optimize_BTSP_CA1
-#SBATCH -o parallel_optimize_BTSP_CA1_112917_full.%j.o
-#SBATCH -e parallel_optimize_BTSP_CA1_112917_full.%j.e
+#SBATCH -o parallel_optimize_BTSP_CA1_120117_full.%j.o
+#SBATCH -e parallel_optimize_BTSP_CA1_120117_full.%j.e
 #SBATCH -p regular
 #SBATCH -N 240
 #SBATCH -L SCRATCH
 #SBATCH -C haswell
 #SBATCH -t 18:00:00
+#SBATCH --qos=premium
 #SBATCH --mail-user=aaronmil@stanford.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -23,5 +24,5 @@ sleep 1
 sleep 60
 srun -N 238 -n 7600 -c 2 --cpu_bind=cores ipengine --mpi=mpi4py --cluster-id=$cluster_id &
 sleep 1
-sleep 300
+sleep 600
 srun -N 1 -n 1 -c 2 --cpu_bind=cores python parallel_optimize.py --config-file-path='data/parallel_optimize_BTSP_CA1_v3_full_config.yaml' --cluster-id=$cluster_id --pop-size=200 --max-iter=50 --path-length=3 --disp --export --output-dir=$SCRATCH/BTSP
