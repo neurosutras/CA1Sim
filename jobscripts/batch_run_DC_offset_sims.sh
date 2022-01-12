19fbe62bf441869b018b88aad415b0942ef118f0
Aaron D. Milstein <neurosutras@gmail.com>
2022-01-12 13:37:10 -0500

    20220112:  - Running some simulations to mimic optogenetic manipulation of somatic Vm.

---
+++ jobscripts/batch_run_DC_offset_sims.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+export DATE=$(date +%Y%m%d_%H%M%S)
+export LABEL=CA1Sim_DC_offset
+export JOB_NAME="$LABEL"_"$DATE"
+sbatch <<EOT
+#!/bin/bash -l
+#SBATCH -J $JOB_NAME
+#SBATCH -o /scratch1/06441/aaronmil/logs/CA1Sim/$JOB_NAME.%j.o
+#SBATCH -e /scratch1/06441/aaronmil/logs/CA1Sim/$JOB_NAME.%j.e
+#SBATCH -p development
+#SBATCH -N 1
+#SBATCH -n 56
+#SBATCH -t 1:00:00
+#SBATCH --mail-user=neurosutras@gmail.com
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+set -x
+
+# cd $WORK2/CA1Sim
+
+declare -a offset=(-0.6 -0.4 -0.2 0 0.2 0.4 0.6)
+
+arraylength=${#offset[@]}
+
+for ((i=0; i<${arraylength}; i++))
+do
+  echo ibrun -n 1 python3 20220112_simulate_place_cell_subtr_inh_record_spines_DC_offset.py 0 $i ${offset[$i]} &
+  let "j = $i + $arraylength"
+  echo ibrun -n 1 python3 20220112_simulate_place_cell_subtr_inh_record_spines_DC_offset.py 1 $j ${offset[$i]} &
+done
+EOT
