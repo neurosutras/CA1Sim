b4ff9bb2bab4d7ea7d8961a0f80d0df535ad7277
Aaron D. Milstein <neurosutras@gmail.com>
2023-11-02 11:43:11 -0400

    20231102:  - Recording Vm and synaptic currents under conditions of somatic DC current offset.

--- jobscripts/process_sim_DC_files_frontera.sh
+++ jobscripts/process_sim_DC_files_frontera.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 export DATE=$(date +%Y%m%d_%H%M%S)
-export JOB_NAME=CA1Sim_DC_offset_"$DATE"
+export JOB_NAME=process_files_CA1Sim_DC_offset_"$DATE"
 sbatch <<EOT
 #!/bin/bash -l
 #SBATCH -J $JOB_NAME
@@ -20,5 +20,5 @@ cd $WORK/CA1Sim
 ibrun -n 1 python3 process_and_merge_i_syn_DC_offset_data.py \
   --source-data-config-file-path=config/20231101_i_syn_DC_offset_file_names.yaml \
   --source-data-dir=$SCRATCH/data/CA1Sim \
-  --target-file-path=data/20231101_merged_i_syn_DC_offset_data.hdf5
+  --target-file-path=$SCRATCH/data/CA1Sim/20231101_merged_i_syn_DC_offset_data.hdf5
 EOT
