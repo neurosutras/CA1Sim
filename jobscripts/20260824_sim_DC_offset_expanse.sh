a38d9abed367e3c45c7fbc8f806684ebe544fc45
Aaron D. Milstein <neurosutras@gmail.com>
2026-08-25 13:14:38 -0400

    20260824: Running simulations for Gritz et al., 2026 revisions

--- jobscripts/20260824_sim_DC_offset_expanse.sh
+++ jobscripts/20260824_sim_DC_offset_expanse.sh
@@ -19,6 +19,8 @@ source $HOME/cpu_py311_intelmpi.sh
 
 cd $PROJECT/CA1Sim
 
+MEM_PER_CPU="1946M"
+
 declare seed=0
 
 for ((i=-2; i<8; i++))
@@ -26,7 +28,8 @@ do
   current=$(awk -v i="$i" 'BEGIN { printf "%.3f", i * 0.026 }')
   for ((j=0; j<10; j++))
   do
-    srun --nodes=1 --ntasks=1 --exclusive python 20231029_simulate_place_cell_record_syn_currents_DC_offset.py $seed $current $SCRATCH/data/CA1Sim &
+    srun --nodes=1 --ntasks=1 -c 2 --mem-per-cpu=$MEM_PER_CPU --exact --exclusive python \
+      20231029_simulate_place_cell_record_syn_currents_DC_offset.py $seed $current $SCRATCH/data/CA1Sim &
     ((++seed))
   done
 done
