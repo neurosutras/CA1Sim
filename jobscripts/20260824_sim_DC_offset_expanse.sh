433cdf642426bc06e099721d4a4d484337846f49
Aaron D. Milstein <neurosutras@gmail.com>
2026-08-25 10:33:38 -0400

    20260824: Running simulations for Gritz et al., 2026 revisions

--- jobscripts/20260824_sim_DC_offset_expanse.sh
+++ jobscripts/20260824_sim_DC_offset_expanse.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -l
 #SBATCH -J CA1Sim_DC_offset
 #SBATCH -o /expanse/lustre/scratch/aaronmil/temp_project/logs/CA1Sim/CA1Sim_DC_offset.%j.o
-#SBATCH -e /expanse/lustre/scratch/aaronmil/temp_project/logs/CA1Sim/CA1Sim_DC_offset.%j.o
+#SBATCH -e /expanse/lustre/scratch/aaronmil/temp_project/logs/CA1Sim/CA1Sim_DC_offset.%j.e
 #SBATCH -p compute
 #SBATCH -N 2
+#SBATCH --ntasks-per-node=50
 #SBATCH -n 100
 #SBATCH -t 6:00:00
 #SBATCH --mem=249208M
@@ -25,7 +26,7 @@ do
   current=$(awk -v i="$i" 'BEGIN { printf "%.3f", i * 0.026 }')
   for ((j=0; j<10; j++))
   do
-    srun -n 1 python 20231029_simulate_place_cell_record_syn_currents_DC_offset.py $seed $current $SCRATCH/data/CA1Sim &
+    srun --nodes=1 --ntasks=1 --exclusive python 20231029_simulate_place_cell_record_syn_currents_DC_offset.py $seed $current $SCRATCH/data/CA1Sim &
     ((++seed))
   done
 done
