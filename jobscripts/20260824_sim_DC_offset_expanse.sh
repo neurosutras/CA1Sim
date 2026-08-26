6f9851bb2fae2eeeb73a15e7c4a1904f2aff2d48
Aaron D. Milstein <neurosutras@gmail.com>
2026-08-26 10:43:18 -0400

    20260824: Running simulations for Gritz et al., 2026 revisions

--- jobscripts/20260824_sim_DC_offset_expanse.sh
+++ jobscripts/20260824_sim_DC_offset_expanse.sh
@@ -3,10 +3,10 @@
 #SBATCH -o /expanse/lustre/scratch/aaronmil/temp_project/logs/CA1Sim/CA1Sim_DC_offset.%j.o
 #SBATCH -e /expanse/lustre/scratch/aaronmil/temp_project/logs/CA1Sim/CA1Sim_DC_offset.%j.e
 #SBATCH -p compute
-#SBATCH -N 7
-#SBATCH --ntasks-per-node=16
+#SBATCH -N 13
+#SBATCH --ntasks-per-node=8
 #SBATCH -n 100
-#SBATCH -c 8
+#SBATCH -c 16
 #SBATCH -t 12:00:00
 #SBATCH --mem=249208M
 #SBATCH --account=sua199
@@ -20,7 +20,7 @@ source $HOME/cpu_py311_intelmpi.sh
 
 cd $PROJECT/CA1Sim
 
-MEM_PER_CPU="2492M"
+MEM_PER_CPU="1946M"
 
 declare seed=0
 
@@ -29,7 +29,7 @@ do
   current=$(awk -v i="$i" 'BEGIN { printf "%.3f", i * 0.026 }')
   for ((j=0; j<10; j++))
   do
-    srun --nodes=1 --ntasks=1 -c 8 --mem-per-cpu=$MEM_PER_CPU --exact --exclusive python \
+    srun --nodes=1 --ntasks=1 -c 16 --mem-per-cpu=$MEM_PER_CPU --exact --exclusive python \
       20231029_simulate_place_cell_record_syn_currents_DC_offset.py $seed $current $SCRATCH/data/CA1Sim &
     ((++seed))
   done
